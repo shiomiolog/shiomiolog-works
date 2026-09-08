883b1eea6584b73724f2cbee10720e6b8f76105f
SHIONE Mio <250790595+shiomiolog@users.noreply.github.com>
2026-09-08 18:48:06 +0900

    Add OGP and polish portfolio design

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,17 @@
 <script setup lang="ts">
+useSeoMeta({
+    title: '汐猫みお Portfolio',
+    description:
+        '情報セキュリティ、情報法、Web開発、執筆、VTuber活動などをまとめた汐猫みおのポートフォリオサイトです。',
+
+    ogTitle: '汐猫みお Portfolio',
+    ogDescription:
+        '情報セキュリティ、情報法、Web開発、執筆、VTuber活動などをまとめたポートフォリオサイトです。',
+    ogImage: 'https://portfolio.shiomiolog.com/images/ogp.png',
+    ogType: 'website',
+
+    twitterCard: 'summary_large_image',
+})
 </script>
 
 <template>
